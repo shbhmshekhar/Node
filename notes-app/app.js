fd52d84a4098ac67c13fec9e351f18a61b345a4c
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-12 14:15:33 +0530

    registering add command to yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,27 @@
 const notes = require('./notes');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+//customize app version using yargs
+yargs.version('1.1.0');
 
-switch (command) {
-  case 'add':
-    console.log('Adding note');
-    break;
-  case 'remove':
-    console.log('Removing note');
-    break;
-}
+// const command = process.argv[2];
+
+// switch (command) {
+//   case 'add':
+//     console.log('Adding note');
+//     break;
+//   case 'remove':
+//     console.log('Removing note');
+//     break;
+// }
+
+// console.log(process.argv);
+
+yargs.command({
+  command: 'add',
+  describe: 'add a new note',
+  handler: function () {
+    console.log('Add a new note');
+  },
+});
