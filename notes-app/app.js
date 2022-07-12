af9edb7026451b0c319de80e60811d6b62ea0060
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-12 13:48:19 +0530

    added chalk npm module to project and created some examples

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,8 @@
 const notes = require('./notes');
 const validator = require('validator');
+const chalk = require('chalk');
 
 console.log('Notes', notes.getNotes());
 
 console.log(validator.isEmail('abc@test.com'));
+console.log(chalk.green.bold.inverse('success!!'));
