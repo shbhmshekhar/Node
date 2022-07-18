330ed1c77b3a2ad37aab61ee2d74dadf94eb3bed
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-18 13:28:05 +0530

    send static files and assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,29 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-  res.send('<h1>Hello World from Express</h1>'); //send HTML back
-  //   res.send('Hello World from Express'); //send text
-});
+app.use(express.static(publicDirPath));
 
-app.get('/help', (req, res) => {
-  //send JSON as a response
-  res.send({
-    name: 'Express App',
-    language_used: 'NodeJS, ExpressJS',
-  });
-});
-app.get('/about', (req, res) => {
-  // send html with CSS as a response
-  res.send(
-    '<h1 style="color: red">About page</h1><p>Details about the application</p>'
-  );
-});
+// app.get('', (req, res) => {
+//   res.send('<h1>Hello World from Express</h1>'); //send HTML back
+//   //   res.send('Hello World from Express'); //send text
+// });
+
+// app.get('/help', (req, res) => {
+//   //send JSON as a response
+//   res.send({
+//     name: 'Express App',
+//     language_used: 'NodeJS, ExpressJS',
+//   });
+// });
+// app.get('/about', (req, res) => {
+//   // send html with CSS as a response
+//   res.send(
+//     '<h1 style="color: red">About page</h1><p>Details about the application</p>'
+//   );
+// });
 app.get('/weather', (req, res) => {
   //   res.send('Weather forecast is coming');
   res.send({
