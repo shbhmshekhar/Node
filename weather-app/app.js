e79e1e704a316aa11f858aa2e90cee98b0deec08
Shubham Shekhar <shbhmshekhar@outlook.com>
2022-07-18 12:33:08 +0530

    destructured objs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,16 @@
-const API_KEY = require('./apikey');
-const request = require('request');
 const chalk = require('chalk');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const placeName = 'Bangalore';
-const weatherServiceUrl = `http://api.weatherstack.com/current?access_key=${API_KEY.WEATHER_STACK_API_KEY}&query=${placeName}`;
-const geoCodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${placeName}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=${API_KEY.MAP_BOX_API_KEY}`;
-
-request({ url: weatherServiceUrl, json: true }, (error, response) => {
+geocode('darbhanga', (error, { longitude, latitude, location } = {}) => {
   if (error) {
-    console.log(chalk.red.inverse('Unable to connect to weather Service'));
-  } else {
-    console.log(response.body.current);
+    return console.log(chalk.red.inverse(error));
   }
-});
-
-request({ url: geoCodeUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log(chalk.red.inverse('Unable to connect to geolocation Service'));
-  } else {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-  }
+  forecast(latitude, longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(chalk.red.inverse(error));
+    }
+    console.log('Location:', location);
+    console.log('Forecast:', chalk.white.inverse(forecastData));
+  });
 });
